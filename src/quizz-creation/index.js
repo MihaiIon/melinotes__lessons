b0210476d16be3185eb9deb2daec48a7116fe981
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2023-04-16 23:16:11 -0400

    refactored createTrebleClefNaturalNoteQuizz function

--- src/quizz-creation/index.js
+++
@@ -1,9 +0,0 @@
-import QuizzModel from "@/models/quizz-model";
-
-import { naturalNotesBetween } from "../helpers";
-
-export const createTrebleClefNaturalNoteQuizz = () => {
-  const allChoices = naturalNotesBetween("A3", "D6");
-
-  return new QuizzModel({ allChoices });
-};
--- src/tests/quizz-creation-helpers/create-treble-clef-natural-note-quizz.test.js
+++ src/tests/quizz-creation-helpers/create-treble-clef-natural-note-quizz.test.js
@@ -1,4 +1,4 @@
-import { createTrebleClefNaturalNoteQuizz } from "@/quizz-creation";
+import { createTrebleClefNaturalNoteQuizz } from "@/quizz-creation-helpers";
 import QuizzModel from "@/models/quizz-model";
 
 describe("createTrebleClefNaturalNoteQuizz", () => {
@@ -9,15 +9,9 @@ describe("createTrebleClefNaturalNoteQuizz", () => {
   });
 
   describe("by default", () => {
-    it('should generate 4 quizz choices', () => {
-      const quizz = createTrebleClefNaturalNoteQuizz();
-
-      expect(quizz.choices.length).toBe(4);
-      expect(quizz.config.choicesCount).toBe(4);
-    });
-
     it('should contain only natural notes', () => {
       const quizz = createTrebleClefNaturalNoteQuizz();
+
       const allChoicesValue = quizz.config.allChoices.map(choice => choice.value);
       const allChoicesValueSet = new Set(allChoicesValue);
 
@@ -32,17 +26,17 @@ describe("createTrebleClefNaturalNoteQuizz", () => {
       expect(allChoicesScientificPitchNotationSet).toEqual(new Set(["A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "B5", "C6", "D6"]));
     });
 
-    it('should select an answer from the quizz choices (100 tries)', () => {
-      let quizz;
-      let choicesValue;
+    it('should generate 4 distinct choices (100 tries)', () => {
+      let quizz, choicesValue, choicesValueSet;
 
       for (let i = 0; i < 100; i++) {
         quizz = createTrebleClefNaturalNoteQuizz();
 
         choicesValue = quizz.choices.map(choice => choice.value);
+        choicesValueSet = new Set(choicesValue);
 
-        expect(choicesValue).toContain(quizz.answer.value);
+        expect(choicesValueSet.size).toBe(4);
       }
-    }); 
+    });
   });
 });
