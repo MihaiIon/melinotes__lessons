b68362fc75736c379367f7d86126fcdad9c21a06
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2023-04-18 19:46:32 -0400

    added more validations on NoteModel

--- src/tests/models/note-model.test.ts
+++ src/tests/models/note-model.test.ts
@@ -22,7 +22,7 @@ describe("NoteModel", () => {
   });
 
   describe(".createFromScientificPitchNotation", () => {
-    describe("when providing a valid scientific pitch notation", () => {
+    describe("when providing a valid scientific pitch notation (natural note)", () => {
       it("should create a note with the value and the octave", () => {
         const note = Model.createFromScientificPitchNotation("C5");
         expect(note.value).toBe("C");
@@ -32,9 +32,59 @@ describe("NoteModel", () => {
       });
     });
 
-    describe("when providing an invalid scientific pitch notation", () => {
+    describe("when providing a valid scientific pitch notation (flat note)", () => {
+      it("should create a note with the value and the octave", () => {
+        const note = Model.createFromScientificPitchNotation("Bb4");
+        expect(note.value).toBe("Bb");
+        expect(note.octave).toBe(4);
+        expect(note.scientificPitchNotation).toBe("Bb4");
+        expect(note.vexflowNotation).toBe("bb/4");
+      });
+    });
+
+    describe("when providing a valid scientific pitch notation (sharp note)", () => {
+      it("should create a note with the value and the octave", () => {
+        const note = Model.createFromScientificPitchNotation("F#3");
+        expect(note.value).toBe("F#");
+        expect(note.octave).toBe(3);
+        expect(note.scientificPitchNotation).toBe("F#3");
+        expect(note.vexflowNotation).toBe("f#/3");
+      });
+    });
+
+    describe("when providing an invalid scientific pitch notation (first letter is lower case)", () => {
       it("should throw an error", () => {
-        expect(() => Model.createFromScientificPitchNotation("C")).toThrowError("Invalid scientific pitch notation");
+        expect(() => Model.createFromScientificPitchNotation("c")).toThrowError(Model.SCIENTIFIC_PITCH_NOTATION_ERROR_MESSAGE);
+      });
+    });
+
+    describe("when providing an invalid scientific pitch notation (does not start with a letter)", () => {
+      it("should throw an error", () => {
+        expect(() => Model.createFromScientificPitchNotation("123")).toThrowError(Model.SCIENTIFIC_PITCH_NOTATION_ERROR_MESSAGE);
+      });
+    });
+
+    describe("when providing an invalid scientific pitch notation (unknown symbol)", () => {
+      it("should throw an error", () => {
+        expect(() => Model.createFromScientificPitchNotation("C$4")).toThrowError(Model.SCIENTIFIC_PITCH_NOTATION_ERROR_MESSAGE);
+      });
+    });
+    
+    describe("when providing an invalid scientific pitch notation (missing octave)", () => {
+      it("should throw an error", () => {
+        expect(() => Model.createFromScientificPitchNotation("C")).toThrowError(Model.SCIENTIFIC_PITCH_NOTATION_ERROR_MESSAGE);
+      });
+    });
+
+    describe("when providing an invalid scientific pitch notation (negative octave)", () => {
+      it("should throw an error", () => {
+        expect(() => Model.createFromScientificPitchNotation("C-4")).toThrowError(Model.SCIENTIFIC_PITCH_NOTATION_ERROR_MESSAGE);
+      });
+    });
+
+    describe("when providing an invalid scientific pitch notation (double digit octave)", () => {
+      it("should throw an error", () => {
+        expect(() => Model.createFromScientificPitchNotation("C44")).toThrowError(Model.SCIENTIFIC_PITCH_NOTATION_ERROR_MESSAGE);
       });
     });
   });
